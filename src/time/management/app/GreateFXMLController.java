1d1624865e3a1edc440f36dfa6d61b2e1f83867b
Mushtak <mosht9876@gmail.com>
2023-07-26 19:33:30 +0300

    Initial commit

---
+++ src/time/management/app/GreateFXMLController.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package time.management.app;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+/**
+ * FXML Controller class
+ *
+ * @author musht
+ */
+public class GreateFXMLController implements Initializable {
+
+    @FXML
+    private TextField name;
+    @FXML
+    private TextField username;
+    @FXML
+    private PasswordField password;
+    @FXML
+    private PasswordField rePassword;
+    @FXML
+    private Button backBt;
+    @FXML
+    private Button signUpBt;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void BackMeethod(ActionEvent event) {
+    }
+
+    @FXML
+    private void signUpMethod(ActionEvent event) {
+    }
+    
+}
